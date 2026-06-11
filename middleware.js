23d523deb189babc6dd0aa8800e0d667a9bde443
Akilesh <akileshsekar2005@gmail.com>
2026-06-12 00:20:57 +0530

    bypass inngest middleware

---
+++ middleware.js
@@ -0,0 +1,55 @@
+import arcjet, { createMiddleware, detectBot, shield } from "@arcjet/next";
+import { clerkMiddleware, createRouteMatcher } from "@clerk/nextjs/server";
+import { NextResponse } from "next/server";
+
+const isProtectedRoute = createRouteMatcher([
+  "/dashboard(.*)",
+  "/account(.*)",
+  "/transaction(.*)",
+]);
+
+const aj = arcjet({
+  key: process.env.ARCJET_KEY,
+  rules: [
+    shield({
+      mode: "LIVE",
+    }),
+    detectBot({
+      mode: "LIVE",
+      allow: [
+        "CATEGORY:SEARCH_ENGINE",
+        "GO_HTTP",
+      ],
+    }),
+  ],
+});
+
+const clerk = clerkMiddleware(async (auth, req) => {
+  const { userId } = await auth();
+
+  if (!userId && isProtectedRoute(req)) {
+    const { redirectToSignIn } = await auth();
+    return redirectToSignIn();
+  }
+
+  return NextResponse.next();
+});
+
+const middleware = createMiddleware(aj, clerk);
+
+export default function customMiddleware(req: Request) {
+  const url = new URL(req.url);
+
+  if (url.pathname.startsWith("/api/inngest")) {
+    return NextResponse.next();
+  }
+
+  return middleware(req as any);
+}
+
+export const config = {
+  matcher: [
+    "/((?!_next|[^?]*\\.(?:html?|css|js(?!on)|jpe?g|webp|png|gif|svg|ttf|woff2?|ico|csv|docx?|xlsx?|zip|webmanifest)).*)",
+    "/(api|trpc)(.*)",
+  ],
+};
